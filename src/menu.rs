95c88fcba0b49527160a1c608547b779a70a1e6c
Sławomir Zborowski <slawek@slawomir.net>
2022-09-24 13:30:13 +0200

    added menu background

--- src/menu.rs
+++ src/menu.rs
@@ -1,6 +1,8 @@
 use macroquad::experimental::scene::{Node, RefMut};
 use macroquad::prelude::*;
 
+const BORDER: u32 = 5;
+
 #[derive(PartialEq, Eq, Clone, Copy, Debug)]
 pub enum MenuSelection {
     StartGame,
@@ -21,10 +23,45 @@ impl Menu {
     }
 }
 
+use crate::{
+    COLOR_ORANGEY, COLOR_ORANGEY_LIGHT, COLOR_TEALY, COLOR_TEALY_LIGHT, SCREEN_HEIGHT, SCREEN_WIDTH,
+};
+
 impl Node for Menu {
     fn ready(_node: RefMut<Self>) {}
 
     fn draw(node: RefMut<Self>) {
+        draw_rectangle(
+            0.,
+            0.,
+            SCREEN_WIDTH as f32,
+            SCREEN_HEIGHT as f32 / 2.,
+            COLOR_TEALY_LIGHT,
+        );
+        draw_rectangle(
+            BORDER as f32,
+            BORDER as f32,
+            (SCREEN_WIDTH - 2 * BORDER) as f32,
+            (SCREEN_HEIGHT - 2 * BORDER) as f32 / 2.,
+            COLOR_TEALY,
+        );
+
+        draw_rectangle(
+            0.,
+            SCREEN_HEIGHT as f32 / 2.,
+            SCREEN_WIDTH as f32,
+            SCREEN_HEIGHT as f32 / 2.,
+            COLOR_ORANGEY_LIGHT,
+        );
+
+        draw_rectangle(
+            BORDER as f32,
+            (SCREEN_HEIGHT / 2) as f32,
+            (SCREEN_WIDTH - 2 * BORDER) as f32,
+            (SCREEN_HEIGHT / 2 - BORDER) as f32,
+            COLOR_ORANGEY,
+        );
+
         draw_text_ex(
             format!("menu selection: {}", node.index).as_str(),
             32.,
